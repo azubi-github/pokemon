fa5d664f8fe5649551548ab93d70460c1642a511
lukas_langer <lukaslanger17@gmail.com>
2024-09-13 13:44:10 +0200

    Pokemon player_team fixed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from pokemon import Pokemon
-from pokemonlist import POKEMON_DATA
+from pokemonlist import POKEMON_DATA, POKEMON_ATTACK
 from pokemonteam import PokemonTeam
 import random
 
@@ -8,56 +8,54 @@ player_team = PokemonTeam()
 player_team_names = []
 enemy_team = PokemonTeam()
 enemy_team_names = []
-
+random_pokemon = []
 player_choosing = True
 
-#with open("C:\\Users\\lukas_langer\\Desktop\\PycharmProjects\\Pokemon\\pokemonlist.txt", "r") as file:
- #   pokemon_file = file.readlines()
-
 print("Build your Team ")
-
+pokemon_found = True
 while player_choosing:
     pokemon_name = input("Pokemon name: ").strip().capitalize()
-
-    pokemon_found = False
-
     for line in POKEMON_DATA.keys():
-
         if pokemon_name == POKEMON_DATA[line]['name']:
-
             if pokemon_name in player_team_names:
                 print(f"{pokemon_name} is already in your Team ")
                 break
-
             else:
-                choice = Pokemon(*POKEMON_DATA.keys())
+                choice = Pokemon(name=POKEMON_DATA[pokemon_name]['name'], element=POKEMON_DATA[pokemon_name]['element'],
+                                 hp=POKEMON_DATA[pokemon_name]['health'], dev=POKEMON_DATA[pokemon_name]['defense'],
+                                 spd=POKEMON_DATA[pokemon_name]['speed'], ability=POKEMON_ATTACK['Vine_Whip'],
+                                 atk=POKEMON_DATA[pokemon_name]['attack'])
                 print(choice)
                 player_team.add_pokemon(choice)
-                player_team_names.append(POKEMON_DATA[pokemon_name])
+                player_team_names.append(pokemon_name)
                 print(f'{pokemon_name} added to your Team ')
-            pokemon_found = True
+                break
+        else:
+            print('Pokemon isnt avalible ')
             break
-
     another = input("Another Pokemon? (yes/no) ").lower().strip()
+    if another == "yes":
+        player_choosing = True
+    else:
+        break
+    if len(player_team_names) <= 0:
+        print("Atleast 1 Pokemon needed ")
+        player_choosing = True
 
-    if another != "yes":
-        player_choosing = False
 
 enemy_team_number = len(player_team_names)
-#funkt noch net
-print(enemy_team_number)
-random_pokemon = []
-#funkt noch net
 for x in range(enemy_team_number):
-    temphold = random.choice(list(POKEMON_DATA.items()))
-    print(temphold)
+    temphold = random.choice(list(POKEMON_DATA.keys()))
     random_pokemon.append(temphold)
-print(random_pokemon)
+
+pokemon_list = []
 for line in random_pokemon:
-    pokemon_list = line.strip().split(",")
-    choice = Pokemon(*pokemon_list)
+    choice = Pokemon(name=POKEMON_DATA[line]['name'], element=POKEMON_DATA[line]['element'],
+                     hp=POKEMON_DATA[line]['health'], dev=POKEMON_DATA[line]['defense'],
+                     spd=POKEMON_DATA[line]['speed'], ability=POKEMON_ATTACK['Vine_Whip'],
+                     atk=POKEMON_DATA[line]['attack'])
     enemy_team.add_pokemon(choice)
-    enemy_team_names.append(pokemon_list[0])
+    enemy_team_names.append(POKEMON_DATA[line]['name'])
 
 print(player_team)
 
@@ -69,10 +67,3 @@ if ask_fight == "yes":
 
 else:
     print("Bye ")
-
-
-while fighting:
-    pass
-
-
-
--- pokemonlist.py
+++ pokemonlist.py
@@ -1,17 +1,17 @@
 import pygame
 
 POKEMON_DATA = {
-    'Charmander':  {'name': 'Charmander','element': 'fire', 'health': 188, 'attack': 112, 'defense': 94, 'speed': 122},
-    'Bulbasaur': {'name': 'Bulbasaur','element': 'plant', 'health': 200, 'attack': 121, 'defense': 121, 'speed': 85},
-    'Squirtle': {'name': 'Squirtle','element': 'water', 'health': 198, 'attack': 94, 'defense': 121, 'speed': 81}
+    'Charmander':  {'name': 'Charmander', 'element': 'Fire', 'health': 188, 'attack': 112, 'defense': 94, 'speed': 122},
+    'Bulbasaur': {'name': 'Bulbasaur', 'element': 'Grass', 'health': 200, 'attack': 121, 'defense': 121, 'speed': 85},
+    'Squirtle': {'name': 'Squirtle', 'element': 'Water', 'health': 198, 'attack': 94, 'defense': 121, 'speed': 81}
 }
 POKEMON_ATTACK = {
-    'Water_Gun': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'water'},
-    'Ember': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'fire'},
-    'Vine_Whip': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'plant'}
+    'Water_Gun': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'Water'},
+    'Ember': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'Fire'},
+    'Vine_Whip': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'Grass'}
 }
 ELEMENT_TYPE = {
-    'Fire': {'fire': 1, 'plant': 2, 'water': 0.5, 'normal': 1},
-    'water': {'fire': 2, 'plant': 0.5, 'water': 1, 'normal': 1},
-    'plant': {'fire': 0.5, 'plant': 1, 'water': 2, 'normal': 1}
+    'Fire': {'Fire': 1, 'Grass': 2, 'Water': 0.5, 'Normal': 1},
+    'Water': {'Fire': 2, 'Grass': 0.5, 'Water': 1, 'Normal': 1},
+    'Plant': {'Fire': 0.5, 'Grass': 1, 'Water': 2, 'Normal': 1}
 }
