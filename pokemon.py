32d00c8b9e8f11be30d8556abdf880e0346feff5
ricardino_hennig <ricardino2003@hotmail.de>
2024-09-25 17:00:23 +0200

    functionalbe ability distribution

--- pokemon.py
+++ pokemon.py
@@ -35,15 +35,22 @@ class Pokemon:
     def get_name(self):
         return self.__name
 
+    def get_ability_list(self):
+        ability_list = []
+        for x in range(3):
+            choice = random.choice(list(POKEMON_ATTACK[self.__element]))
+            if choice in ability_list:
+                while choice in ability_list:
+                    choice = random.choice(list(POKEMON_ATTACK[self.__element]))
+                ability_list.append(choice)
+            else:
+                ability_list.append(choice)
+        ability_list.append(random.choice(list(POKEMON_ATTACK["normal"])))
+        print(ability_list)
+        return ability_list
+
     def __repr__(self):
         return (f'{self.__name}, {self.__element}, HP: {self.__hp}, '
                 f'ATK: {self.__atk}, DEF: {self.__dev}, SPD: {self.__spd},'
                 f'Abilities: {self.__ability}')
-
-    def get_ability_list(self):
-        ability_list = []
-        for x in range(3):
-            ability_list.append(random.choice(list(POKEMON_ATTACK[self.__element])))
-        ability_list.append(random.choice(POKEMON_ATTACK["normal"]))
-        print(ability_list)
-        return ability_list
+print('test')
--- pokemonlist.py
+++ pokemonlist.py
@@ -9,7 +9,7 @@ POKEMON_ATTACK = {
     'normal': {'tackle': {'Attack_Strength': 30, 'Accuracy': 100, 'element': 'normal'}, 'MEME-BEAM': {'Attack_Strength': 4000000, 'Accuracy': 100, 'element': 'Normal'}, },
     'Water': {'Water_Gun': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'Water'},'Wet-Fart': {'Attack_Strength': 60, 'Accuracy': 80, 'element': 'Water'}, 'Nasses-Handtuch': {'Attack_Strength': 80, 'Accuracy': 60, 'element': 'Water'}, },
     'Fire': {'Ember': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'Fire'}, 'Deo+Feuerzeug': {'Attack_Strength': 60, 'Accuracy': 80, 'element': 'Fire'}, 'Indisches_Chili': {'Attack_Strength': 80, 'Accuracy': 60, 'element': 'Fire'} },
-    'Plant': {'Vine_Whip': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'Grass'}, 'Brenneseln': {'Attack_Strength': 60, 'Accuracy': 80, 'element': 'Grass'}, 'Kastanien_Attentat': {'Attack_Strength': 80, 'Accuracy': 60, 'element': 'Grass'} },
+    'Grass': {'Vine_Whip': {'Attack_Strength': 40, 'Accuracy': 100, 'element': 'Grass'}, 'Brenneseln': {'Attack_Strength': 60, 'Accuracy': 80, 'element': 'Grass'}, 'Kastanien_Attentat': {'Attack_Strength': 80, 'Accuracy': 60, 'element': 'Grass'} },
 }
 ELEMENT_TYPE = {
     'Fire': {'Fire': 1, 'Grass': 2, 'Water': 0.5, 'Normal': 1},
