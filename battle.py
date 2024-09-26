d50b1ff89fab71f3470c3410178880bcb74fec48
ricardino_hennig <ricardino2003@hotmail.de>
2024-09-26 18:04:02 +0200

    Enemy Pokemon doesnt attack needs to be fixed, abilitys work for user pokemon. Additional Dic for new Attack functin

--- battle.py
+++ battle.py
@@ -1,3 +1,6 @@
+import pokemon
+from pokemonlist import POKEMON_ATTACK_VALUES
+
 
 class Battle:
     def __init__(self, enemy_active_pokemon, player_active_pokemon):
@@ -5,8 +8,8 @@ class Battle:
         self.player_pokemon = player_active_pokemon
 
     def start_battle(self, enemy_active_pokemon, player_active_pokemon):
-        print(f'Enemy Pokemon: {enemy_active_pokemon}')
-        print(f'Player Pokemon: {player_active_pokemon}')
+        print(f'Enemy Pokemon: {enemy_active_pokemon.get_name()} ')
+        print(f'Player Pokemon: {player_active_pokemon.get_name()} ')
 
     def check_fainted(self, player_active_pokemon, enemy_active_pokemon):
         if player_active_pokemon.get_current_hp() <= 0:
@@ -17,15 +20,16 @@ class Battle:
             return True
 
     def attack_selection(self, player_active_pokemon, enemy_active_pokemon):
-        attack = input('choose your attack: Ember, Vine Whip, Water Gun (1-3) ')
-        if attack == '1':
-            self.attack_enemy(player_active_pokemon, enemy_active_pokemon)
-        elif attack == '2':
-            self.attack_enemy(player_active_pokemon, enemy_active_pokemon)
-        elif attack == '3':
-            self.attack_enemy(player_active_pokemon, enemy_active_pokemon)
-        else:
-            print('Choose between 1-3 ! ')
+        abilities = player_active_pokemon.get_ability_list()
+        while True:
+            if len(abilities) > 1:
+                choice = int(input(f'choose your attack: {abilities} (1-{len(abilities)}) '))
+                if choice < 1 or choice > len(abilities):
+                    print('Ability not avalible ')
+                else:
+                    ability = player_active_pokemon.get_ability_list()[choice - 1]
+                    print(ability)
+                    self.attack_enemy(player_active_pokemon, enemy_active_pokemon, ability)
 
     def enemy_turn(self, player_active_pokemon, enemy_active_pokemon):
         enemy_active_pokemon.attack_player(player_active_pokemon, enemy_active_pokemon)
@@ -39,14 +43,15 @@ class Battle:
     def flee(self, player_active_pokemon, enemy_active_pokemon):
         if player_active_pokemon.get_speed() >= enemy_active_pokemon.get_speed():
             print('You escaped succesfully ')
-            return False
+            return True
         elif player_active_pokemon.get_speed() <= enemy_active_pokemon.get_speed():
             print('You could´nt escape ')
+            return False
 
     def display_actions(self, player_active_pokemon, enemy_active_pokemon):
         print(
             f'{player_active_pokemon.get_name()} (HP: {player_active_pokemon.get_current_hp()}) '
-            f'vs {enemy_active_pokemon.get_name()} (HP: {enemy_active_pokemon.get_current_hp()})')
+            f'vs {enemy_active_pokemon.get_name()} (HP: {enemy_active_pokemon.get_current_hp()}) ')
         print('Choose an action: ')
         selection = input('1. Attack , 2. Switch, 3. Bag, 4. Flee ')
         if selection == '1':
@@ -58,8 +63,9 @@ class Battle:
         elif selection == '4':
             self.flee(player_active_pokemon, enemy_active_pokemon)
 
-    def attack_enemy(self, player_active_pokemon, enemy_active_pokemon):
-        damage = player_active_pokemon.get_atk() - enemy_active_pokemon.get_defense()
+    def attack_enemy(self, player_active_pokemon, enemy_active_pokemon, ability):
+
+        damage = player_active_pokemon.get_atk() * (0.1 * (int(POKEMON_ATTACK_VALUES[ability]['Attack_Strength']))) / (1 + (enemy_active_pokemon.get_defense() / 200))
         damage = int(max(1, damage))
         enemy_active_pokemon.take_damage(damage)
         print(f'{player_active_pokemon.get_name()} attacked {enemy_active_pokemon.get_name()} for {damage} damage! ')
